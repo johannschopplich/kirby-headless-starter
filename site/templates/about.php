de0a6848b35138eea6ff2fa8f430e30c9b9cf00e
Johann Schopplich <mail@johannschopplich.com>
2022-05-30 14:46:21 +0200

    chore: indentation

--- site/templates/about.php
+++ site/templates/about.php
@@ -3,12 +3,12 @@
 /** @var \Kirby\Cms\Page $page */
 
 $data = [
-    'title' => $page->title()->value(),
-    'layout' => $page->layout()->toLayouts()->toArray(),
-    'address' => $page->address()->value(),
-    'email' => $page->email()->value(),
-    'phone' => $page->phone()->value(),
-    'social' => $page->social()->toStructure()->toArray()
+  'title' => $page->title()->value(),
+  'layout' => $page->layout()->toLayouts()->toArray(),
+  'address' => $page->address()->value(),
+  'email' => $page->email()->value(),
+  'phone' => $page->phone()->value(),
+  'social' => $page->social()->toStructure()->toArray()
 ];
 
 echo \Kirby\Data\Json::encode($data);
