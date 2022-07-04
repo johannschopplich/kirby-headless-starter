be95bb9c63aedabce40fadd0e2b81058cb3e122b
Johann Schopplich <mail@johannschopplich.com>
2022-07-04 11:48:05 +0200

    refactor: simplify frontend url site method

--- site/plugins/headless/src/extensions/siteMethods.php
+++ site/plugins/headless/src/extensions/siteMethods.php
@@ -3,12 +3,6 @@
 return [
     'frontendUrl' => function () {
         /** @var \Kirby\Cms\Site $this */
-        $frontendUrl = env('KIRBY_HEADLESS_FRONTEND_URL');
-
-        if (empty($frontendUrl)) {
-            return null;
-        }
-
-        return $frontendUrl;
+        return env('KIRBY_HEADLESS_FRONTEND_URL', null);
     }
 ];
