3dbcb04f34ea8cdadfc34e22cfe81d4b0e840c20
Johann Schopplich <mail@johannschopplich.com>
2022-11-16 18:26:38 +0100

    feat: add `*` fallback to allow origin header

--- site/plugins/headless/src/classes/Api.php
+++ site/plugins/headless/src/classes/Api.php
@@ -54,7 +54,7 @@ class Api
         }
 
         return Response::json($body, $code, null, [
-            'Access-Control-Allow-Origin' => env('KIRBY_HEADLESS_ALLOW_ORIGIN')
+            'Access-Control-Allow-Origin' => env('KIRBY_HEADLESS_ALLOW_ORIGIN', '*')
         ]);
     }
 
@@ -96,8 +96,8 @@ class Api
     public static function preflightResponse(): \Kirby\Http\Response
     {
         return new Response('', null, 204, [
-            'Access-Control-Allow-Origin' => env('KIRBY_HEADLESS_ALLOW_ORIGIN'),
-            'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Origin' => env('KIRBY_HEADLESS_ALLOW_ORIGIN', '*'),
+            'Access-Control-Allow-Methods' => env('KIRBY_HEADLESS_ALLOW_METHODS', 'GET, POST, OPTIONS'),
             'Access-Control-Allow-Headers' => '*',
             'Access-Control-Max-Age' => '86400',
             'Content-Length' => '0'
