128252f6ae56bcaa8687e07bdcd1d3b1238a8da3
Johann Schopplich <mail@johannschopplich.com>
2022-12-06 20:06:17 +0100

    feat: resolve image block in layouts

--- site/plugins/headless/src/extensions/fieldMethods.php
+++ site/plugins/headless/src/extensions/fieldMethods.php
@@ -1,35 +1,55 @@
 <?php
 
+$imageBlockResolver = function (\Kirby\Cms\Block $block) {
+    if ($block->type() === 'image') {
+        /** @var \Kirby\Cms\File|null $image */
+        $image = $block->content()->get('image')->toFile();
+
+        if ($image) {
+            $resolvedImage = [
+                'url'    => $image->url(),
+                'width'  => $image->width(),
+                'height' => $image->height(),
+                'srcset' => $image->srcset(),
+                'alt'    => $image->alt()->value()
+            ];
+
+            // Replace the image field with the resolved image
+            $block->content()->update(['image' => $resolvedImage]);
+        }
+    }
+
+    return $block;
+};
+
 return [
     /**
      * Enhances the `toBlocks()` method to resolve images
      *
      * @kql-allowed
      */
-    'toResolvedBlocks' => function (\Kirby\Cms\Field $field) {
+    'toResolvedBlocks' => function (\Kirby\Cms\Field $field) use ($imageBlockResolver) {
         return $field
             ->toBlocks()
-            ->map(function ($block) {
-                /** @var \Kirby\Cms\Block $block */
-                if ($block->type() === 'image') {
-                    /** @var \Kirby\Cms\File|null $image */
-                    $image = $block->content()->get('image')->toFile();
+            ->map($imageBlockResolver);
+    },
 
-                    if ($image) {
-                        $resolvedImage = [
-                            'url'    => $image->url(),
-                            'width'  => $image->width(),
-                            'height' => $image->height(),
-                            'srcset' => $image->srcset(),
-                            'alt'    => $image->alt()->value()
-                        ];
+    /**
+     * Enhances the `toLayouts()` method to resolve images
+     *
+     * @kql-allowed
+     */
+    'toResolvedLayouts' => function (\Kirby\Cms\Field $field) use ($imageBlockResolver) {
+        return $field
+            ->toLayouts()
+            ->map(function (\Kirby\Cms\Layout $layout) use ($imageBlockResolver) {
+                $columns = $layout->columns();
 
-                        // Replace the image field with the resolved image
-                        $block->content()->update(['image' => $resolvedImage]);
-                    }
+                foreach ($columns as $column) {
+                    $column->blocks()->map($imageBlockResolver);
                 }
 
-                return $block;
+                return $layout;
             });
     }
 ];
