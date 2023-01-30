f898a979beba19b35ecfca05ae583dfa44b4546d
Johann Schopplich <mail@johannschopplich.com>
2023-01-30 21:05:21 +0100

    chore: untrack plugins from repo (in favour of Composer install)

--- site/plugins/kql/extensions/aliases.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-class_alias('Kirby\Kql\Kql', 'Kql');
-class_alias('Kirby\Kql\Interceptor', 'Kirby\Kql\Interceptors\Interceptor');
--- site/plugins/kql/src/Kql/Interceptors/Cms/FileVersion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Kirby\Kql\Interceptors\Cms;
-
-class FileVersion extends File
-{
-	public const CLASS_ALIAS = 'file';
-}
