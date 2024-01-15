23f9f2392e6f6106fa94ad5b56259b9ce9ca92d1
Johann Schopplich <mail@johannschopplich.com>
2024-01-15 18:55:08 +0100

    perf: remove `kirby-helpers` dependency

--- public/index.php
+++ public/index.php
@@ -3,7 +3,33 @@
 $base = dirname(__DIR__);
 
 require $base . '/vendor/autoload.php';
-\JohannSchopplich\Helpers\Env::load($base);
+
+if (class_exists(\Dotenv\Dotenv::class) && !function_exists('env')) {
+    function env($key, $default = null)
+    {
+        static $repository = null;
+
+        if ($repository === null) {
+            $base = dirname(__DIR__);
+            $repository = \Dotenv\Repository\RepositoryBuilder::createWithDefaultAdapters()->immutable()->make();
+            \Dotenv\Dotenv::create($repository, $base)->safeLoad();
+        }
+
+        $value = $repository->get($key);
+
+        if ($value === null) {
+            return $default instanceof \Closure ? $default() : $default;
+        }
+
+        return match (strtolower($value)) {
+            'true', '(true)' => true,
+            'false', '(false)' => false,
+            'empty', '(empty)' => '',
+            'null', '(null)' => null,
+            default => preg_match('/\A([\'"])(.*)\1\z/', $value, $matches) ? $matches[2] : $value,
+        };
+    }
+}
 
 $kirby = new \Kirby\Cms\App([
     'roots' => [
