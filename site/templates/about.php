2f366b3a60157bfdfd0b3236e20f09d9e2083a7e
Johann Schopplich <mail@johannschopplich.com>
2025-04-21 18:40:48 +0100

    chore: remove unused `images` data

--- site/templates/about.php
+++ site/templates/about.php
@@ -8,16 +8,7 @@ $data = [
   'address' => $page->address()->kirbytext()->value(),
   'email' => $page->email()->value(),
   'phone' => $page->phone()->value(),
-  'social' => $page->social()->toStructure()->toArray(),
-  'images' => $page
-    ->images()
-    ->map(fn ($i) => [
-      'id' => $i->id(),
-      'uuid' => $i->uuid()->toString(),
-      'url' => $i->url(),
-      'alt' => $i->alt()->value()
-    ])
-    ->values()
+  'social' => $page->social()->toStructure()->toArray()
 ];
 
 echo \Kirby\Data\Json::encode($data);
--- site/templates/note.php
+++ site/templates/note.php
@@ -10,16 +10,7 @@ $data = [
   'published' => $page->date()->toDate('c'),
   'cover' => [
     'url' => ($page->cover()->toFile()?->toArray() ?? $page->images()->first()?->toArray() ?? [])['url'] ?? '',
-  ],
-  'images' => $page
-    ->images()
-    ->map(fn ($i) => [
-      'id' => $i->id(),
-      'uuid' => $i->uuid()->toString(),
-      'url' => $i->url(),
-      'alt' => $i->alt()->value()
-    ])
-    ->values()
+  ]
 ];
 
 echo \Kirby\Data\Json::encode($data);
