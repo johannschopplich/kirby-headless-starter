82dde00720b346337bef3f6b9b5ecc295393d618
Johann Schopplich <johann@schopplich.com>
2026-07-01 22:54:47 +0200

    refactor: align headless config and enable panel redirect

--- site/config/config.php
+++ site/config/config.php
@@ -43,7 +43,6 @@ return [
         ]
     ],
 
-    // Blocks resolver configuration
     // See: https://kirby.tools/docs/headless/usage/field-methods#toresolvedblocks
     'blocksResolver' => require __DIR__ . '/blocks-resolver.php',
 
@@ -56,34 +55,26 @@ return [
         }
     ],
 
-    // Default to token-based authentication
     'kql' => [
         'auth' => 'bearer'
     ],
 
-    // Kirby 5 native CORS support
     'cors' => [
         'allowOrigin' => env('KIRBY_CORS_ALLOW_ORIGIN', '*'),
-        'allowMethods' => ['GET', 'HEAD', 'POST', 'OPTIONS'],
         'allowHeaders' => true,
         'maxAge' => 86400
     ],
 
-    // Kirby headless options
+    // See: https://kirby.tools/docs/headless/configuration/authentication
     'headless' => [
-        // Enable returning Kirby templates as JSON
         'globalRoutes' => true,
 
-        // Optional API token to use for authentication, also used
-        // for for KQL endpoint
         'token' => env('KIRBY_HEADLESS_API_TOKEN'),
 
         'panel' => [
             // Preview URL for the Panel preview button
             'frontendUrl' => env('KIRBY_HEADLESS_FRONTEND_URL'),
-            // Redirect to the Panel if no authorization header is sent,
-            // useful for editors visiting the site directly
-            'redirect' => false
+            'redirect' => true
         ]
     ]
 
