5ecd5375f32bf62469616f572cbfced790ff3451
Johann Schopplich <johann@schopplich.com>
2026-07-01 22:54:47 +0200

    chore(deploy): drop node build step from ploi script

--- scripts/ploi-deploy.sh
+++ scripts/ploi-deploy.sh
@@ -12,20 +12,6 @@ composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 
 {RELOAD_PHP_FPM}
 
-# Ensure NVM is loaded
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Enable Corepack and install pnpm
-corepack enable
-
-# Install dependencies and build the project
-if [ -f package-lock.json ]; then
-  npm ci && npm run build
-elif [ -f pnpm-lock.yaml ]; then
-  pnpm i && pnpm run build
-fi
-
 # Clean Kirby cache
 rm -rf storage/cache/{SITE_DOMAIN}
 
